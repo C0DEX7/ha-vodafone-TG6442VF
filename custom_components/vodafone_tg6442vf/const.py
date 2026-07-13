0b0924b21970d7f35f7b2bb1598761b79c7c3fa7
C0D3X <103962484+C0DEX7@users.noreply.github.com>
2026-07-13 19:46:59 +0200

    Create const.py

---
+++ custom_components/vodafone_tg6442vf/const.py
@@ -0,0 +1,10 @@
+"""Constants for the Vodafone Station Custom integration."""
+from typing import Final
+
+DOMAIN: Final = "vodafone_tg6442vf"
+
+CONF_HOST: Final = "host"
+CONF_USERNAME: Final = "username"
+CONF_PASSWORD: Final = "password"
+
+UPDATE_INTERVAL: Final = 30  # Polling rate in seconds
